9d4ff09a3c03d5b7ccdb4947410d7bc3189446d8
Alex Novikov <alex@asnovikov.ru>
2021-10-14 19:53:25 -0300

    switched to https;

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(80);
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/private.key'),
+    cert: fs.readFileSync('./secrets/certificate.crt'),
+  };
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    httpsOptions,
+  });
+  await app.listen(443);
 }
 bootstrap();
