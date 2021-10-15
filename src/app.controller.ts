e43ab3634ce0799ebf652574ceb75be6fc40df2b
Alex Novikov on mac-mini <alex@asnovikov.ru>
2021-10-15 11:59:01 -0300

    added logging;

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req, All } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
+  name = 'Free TON Notification provider API';
+
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get() getHello(@Req() req: Request): string {
+    console.log(this.appService.explainRequest(req));
+    return `${this.name}: get`;
+  }
+
+  @Post() create(@Req() req: Request): string {
+    console.log(this.appService.explainRequest(req));
+    return `${this.name}: post`;
+  }
+
+  @All() logging(@Req() req: Request): string {
+    console.log(this.appService.explainRequest(req));
+    return `${this.name}: ${req.method}`;
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Free TON Notification provider API';
+  }
+
+  explainRequest(req: Request): string {
+    return `${req.method} ${req.protocol}://${req.hostname}(${req.ip})${
+      req.url
+    } ${JSON.stringify(req.params)}`;
   }
 }
